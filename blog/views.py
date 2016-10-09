ceb0da862a0b78e9d746110329d2f0e1da4448e4
Dasha Fastova <dawa_fastova@mail.ru>
2016-10-09 17:26:03 +0300

    add style and detail page

--- blog/views.py
+++ blog/views.py
@@ -5,3 +5,10 @@ from django.utils import timezone
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    detail = Post.objects.filter(pk=pk).first()
+    return render(request, 'blog/post_detail.html', {'post_detail': detail})
+
+
